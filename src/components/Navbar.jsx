93fdffcbd1a727b5c20d849b7fe42c29a6bad77d
Mohamad Hajjar <69475046+M-Hajjar@users.noreply.github.com>
2024-09-20 20:36:46 +0200

    added favorites button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import './Navbar.css';
 
-const Navbar = ({ categories, onCategoryChange, showCategories = true }) => { // Added showCategories prop with default value true
+const Navbar = ({ categories, onCategoryChange, showCategories = true }) => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   const handleCategorySelect = (category) => {
@@ -14,27 +14,34 @@ const Navbar = ({ categories, onCategoryChange, showCategories = true }) => { //
       <div className="navbar-logo">
         <span className="gojo">Gojo</span><span className="shows">Shows</span>
       </div>
-      {showCategories && ( // Conditionally render the categories button
-        <div 
-          className="navbar-categories-button" 
-          onMouseEnter={() => setIsDropdownOpen(true)} 
-          onMouseLeave={() => setIsDropdownOpen(false)}
-        >
-          Categories
-          {isDropdownOpen && (
-            <ul className="navbar-categories-dropdown">
-              {categories.map((category, index) => (
-                <li 
-                  key={index} 
-                  onClick={() => handleCategorySelect(category)}
-                >
-                  {category}
-                </li>
-              ))}
-            </ul>
-          )}
+      
+      <div className="navbar-buttons">
+        {showCategories && ( // Conditionally render the categories button
+          <div 
+            className="navbar-categories-button" 
+            onMouseEnter={() => setIsDropdownOpen(true)} 
+            onMouseLeave={() => setIsDropdownOpen(false)}
+          >
+            Categories
+            {isDropdownOpen && (
+              <ul className="navbar-categories-dropdown">
+                {categories.map((category, index) => (
+                  <li 
+                    key={index} 
+                    onClick={() => handleCategorySelect(category)}
+                  >
+                    {category}
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
+        )}
+        {/* Add Favourite Shows button */}
+        <div className="navbar-favourites-button">
+          Favourite Shows
         </div>
-      )}
+      </div>
     </nav>
   );
 };
