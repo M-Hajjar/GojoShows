70e76883a7694f0420d8e93b2bb62ffe9dca1585
Mohamad Hajjar <69475046+M-Hajjar@users.noreply.github.com>
2024-09-20 09:40:58 +0200

    Fexing a netlify upload error 2

--- src/pages/AnimeDetail.jsx
+++ src/pages/AnimeDetail.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { fetchAnimeDetails } from '../api/jikanApi';
+import { fetchAnimeDetails } from '../api/JikanApi';
 import Navbar from '../components/Navbar'; // Import Navbar
 import Footer from '../components/Footer';
 import './AnimeDetail.css';
